f9066329aa6206adf2c681ebb5c52a71db188581
Larissa Lucas <larissalucas.efg@gmail.com>
2026-03-31 21:34:25 -0300

    Segundo Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Projeto Big Data com Apache Spark
+
+### Descrição
+Projeto para análise de dados CSV utilizando Apache Spark.
+
+### Pré-requisitos
+- Python 3
+- Apache Spark ou Databricks
+
+### Execução
+1. Criar a pasta `dados`
+2. Colocar o arquivo CSV dentro dela
+3. Executar os scripts da pasta `src`
+
+### Observação
+O arquivo CSV não é versionado no Git.
--- src/processamento.py
+++ src/processamento.py
@@ -1,8 +1,12 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, regexp_replace
+from pyspark.sql.functions import col, regexp_replace, sum, avg, count, desc, round
+import matplotlib.pyplot as plt
 
 spark = SparkSession.builder\
 .appName("Bolsa Familia")\
+.config("spark.driver.memory", "8g") \
+.config("spark.executor.memory", "8g") \
+.config("spark.sql.shuffle.partitions", "50")\
 .getOrCreate()
 
 caminho_csv ="dados/pagamentos.csv"
@@ -13,6 +17,9 @@ df = spark.read\
 .option ("sep", ";")\
 .option ("encoding","ISO-8859-1")\
 .csv(caminho_csv)
+
+df.show(5)
+df.printSchema()
 
 
 ######################################
@@ -71,7 +78,7 @@ for antiga, nova in colunas_padrao.items():
 
 import unicodedata
 import re
-df_tratado = df
+
 def padronizar_nome(col):
     col = col.lower()
     col = unicodedata.normalize("NFD", col)
@@ -101,6 +108,10 @@ df_tratado = df_tratado.withColumn(
     col("valor_parcela").cast("decimal(10,2)")
 )
 
+##################################################
+#  TRATAMENTO AUTOMÁTICO - SIMPLIFICADO
+##################################################
+
 df_tratado = (
     df_tratado
     .dropna()
@@ -111,8 +122,64 @@ df_tratado = (
     "valor_parcela",
     col("valor_parcela").cast("decimal(10,2)"))
 )
-df_tratado.show(10)
+df_tratado.show(5)
 df_tratado.printSchema()
+
+##################################################
+#  SOMA E MÉDIA 
+##################################################
+
+df_tratado.agg(
+    round(sum("valor_parcela"), 2).alias("total_pago"),
+    round(avg("valor_parcela"), 2).alias("media_pagamento")
+).show()
+
+##################################################
+#  AGRUPANDO POR UF EM ORDEM DECRESCENTE
+##################################################
+
+df_tratado.groupBy("uf")\
+    .agg(sum("valor_parcela").alias("total_pago"))\
+    .orderBy("total_pago" , ascending=False)\
+    .show(26)
+
+##################################################
+#  RANKING TOP 10
+##################################################
+
+ranking_favorecido = df_tratado.groupBy("cpf_favorecido" , "nome_favorecido")\
+    .agg(
+        sum("valor_parcela").alias("valor_total_acumulado"),
+        count("valor_parcela").alias("quantidade_parcelas")
+    )\
+    .orderBy(desc("valor_total_acumulado"))
+
+print("Rankink dos 10 primeiros com valores acumulados")
+ranking_favorecido.show(5, truncate=False)
+
+##################################################
+#  MEDIA GERAL POR PARCELA
+##################################################
+
+media_geral = df_tratado.agg(avg("valor_parcela")).collect()[0][0]
+print(f"Para fins de comparação, media geral de cada parcela e R$ {media_geral:.2f}")
+
+df_uf = df_tratado.groupBy("uf") \
+    .agg(sum("valor_parcela").alias("total_pago")) \
+    .orderBy("total_pago", ascending=False) \
+    .limit(10) \
+    .toPandas()
+
+##################################################
+#  GRAFICOS
+##################################################
+
+plt.figure()
+plt.bar(df_uf["uf"], df_uf["total_pago"])
+plt.title("Top 10 UFs - Total Pago Bolsa Família")
+plt.xlabel("UF")
+plt.ylabel("Total Pago")
+plt.show()
 
 
 
